0012b0f50504a1ae05228aff3ac1d8d6c6da83b0
Aumberto <aumberto.jimenez@gmail.com>
2015-01-02 17:58:29 +0000

    Cambio en el menú principal
    Cambiamos propuestas en el menú principal

--- app/views/layout/main_layout.blade.php
+++ app/views/layout/main_layout.blade.php
@@ -57,7 +57,7 @@
 <body>
 
 <div class='menudes'>
-  {{HTML::link('propuesta', 'Propuestas')}}
+  {{HTML::link('propuesta', 'Propuesta')}}
   {{HTML::link('traslado', 'Traslados')}}
   {{HTML::link('pedido', 'Pedidos')}}
   {{HTML::link('consumo/proveedores', 'Stock Proveedores')}}
