e4aea38e3f36e31d2784341d17bfbfc03504c5eb
unknown <ajimenez@dyl-p002.ADSA.local>
2015-01-13 18:32:48 +0000

    Mejora #3

--- app/controllers/PedidoController.php
+++ app/controllers/PedidoController.php
@@ -4,7 +4,7 @@ class PedidoController extends BaseController{
 
   public function getIndex(){
 
-  		$pedidos = Pedido::where('estado', '<>', 'Descargado')->get();
+  		$pedidos = Pedido::where('estado', '<>', 'Descargado')->orderby('fecha_descarga')->get();
   		return View::make('pedido.pedido_list')->with('pedidos', $pedidos);
   }
 
