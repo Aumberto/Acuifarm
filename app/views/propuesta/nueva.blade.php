16f857410b353136591aeff5b2c9437e2592d494
unknown <ajimenez@dyl-p002.ADSA.local>
2015-01-29 17:57:58 +0000

    Propuesta Listado y Añadir

--- app/views/propuesta/nueva.blade.php
+++ app/views/propuesta/nueva.blade.php
@@ -1,43 +1,44 @@
 @extends('layout.main_layout')
 
 @section('content')
-
+<div class="container">
    <h1>Nueva Propuesta de Alimentación</h1>
    {{Form::open(array('url' => 'propuesta/add', 'class' =>'form-horizontal'))}}
     
 
-    <div class="control-group">
-   {{Form::label('granja_id', 'Granja', array('class' => 'control-label'))}}
-   <div class="controls">
-   	<select name="granja_id" id="granja_id">
-   		@foreach($granjas as $granja)
-          <option value="{{$granja->id}}">{{$granja->nombre}}</option>
-   		@endforeach
-   	</select>
-     
-   </div>
-  </div>
+    <div class="form-group">
+     {{Form::label('granja_id', 'Granja', array('class' => 'col-sm-3 control-label'))}}
+     <div class="col-sm-2">
+   	   <select name="granja_id" id="granja_id" class="form-control input-sm">
+   		   @foreach($granjas as $granja)
+           <option value="{{$granja->id}}">{{$granja->nombre}}</option>
+   		   @endforeach
+   	   </select>
+     </div>
+    </div>
 
-    <div class="control-group">
-      {{Form::label('fecha_ini', 'Fecha Inicial:', array('class' => 'control-label'))}}
-      <div class="controls">
-      	<input type="text" name="fecha_ini" id="fecha_ini">
-        
+    <div class="form-group">
+      {{Form::label('fecha_ini', 'Fecha Inicial:', array('class' => 'col-sm-3 control-label'))}}
+      <div class="col-sm-2">
+      	<input type="text" name="fecha_ini" id="fecha_ini" class="form-control input-sm">
       </div>
     </div>
-    <div class="control-group">
-      {{Form::label('fecha_fin', 'Fecha Final:', array('class' => 'control-label'))}}
-      <div class="controls">
-        
-        <input type="text" name="fecha_fin" id="fecha_fin">
+    
+    <div class="form-group">
+      {{Form::label('fecha_fin', 'Fecha Final:', array('class' => 'col-sm-3 control-label'))}}
+      <div class="col-sm-2">
+        <input type="text" name="fecha_fin" id="fecha_fin" class="form-control input-sm">
       </div>
     </div>
 
-    <div class="control-group">
-      <div class="controls">
-        {{Form::submit('Guardar', array('class' =>'btn'))}}
+    <div class="form-group">
+      <div class="row">
+        <div class="col-md-6 col-md-offset-3">
+        {{Form::submit('Guardar', array('class' =>'btn btn-primary'))}}
+        {{HTML::link('/propuesta', 'Cancelar',  array('class' =>'btn btn-primary'))}}
       </div>
     </div>
+</div>
 
 
 	{{ Form::close() }}
--- app/views/propuesta/propuesta_list.blade.php
+++ app/views/propuesta/propuesta_list.blade.php
@@ -5,32 +5,37 @@
 
 <div class="container">
 <h1>Propuestas de Alimentación</h1>
-{{HTML::link('/propuesta/add', 'Nueva Propuesta')}}
-<table class="table table-hover">
+{{HTML::link('/propuesta/add', 'Nueva Propuesta', array('class' =>'btn btn-primary'))}}
+<br><br>
+ <div class="table-responsive">
+  <table class="table table-striped table-bordered">
   	<thead>
   		<tr>
-  		  <th>Granja</th>
-  		  <th>Descripción</th>
-  		  <th>Desde</th>
-        <th>Hasta</th>
+  		  <th class="text-center">Granja</th>
+  		  <th class="text-center">Descripción</th>
+  		  <th class="text-center">Desde</th>
+        <th class="text-center">Hasta</th>
+        <th class="text-center">Acciones</th>
       </tr>
   	</thead>
+
   	<tbody>
   		@foreach($propuestas as $propuesta)
   		  <tr>
-  		    <td>{{$propuesta->granja}}</td>
-  		    <td>{{$propuesta->descripcion}} </td>
-  		    <td>{{date("d-m-Y",strtotime($propuesta->fecha_ini))}}</td>
-  		    <td>{{date("d-m-Y",strtotime($propuesta->fecha_fin))}}</td>
-  		    <td>
+  		    <td class="text-left">{{$propuesta->granja}}</td>
+  		    <td class="text-left">{{$propuesta->descripcion}} </td>
+  		    <td class="text-center">{{date("d-m-Y",strtotime($propuesta->fecha_ini))}}</td>
+  		    <td class="text-center">{{date("d-m-Y",strtotime($propuesta->fecha_fin))}}</td>
+  		    <td class="text-center">
   		        {{Html::link('propuesta/ver/'. $propuesta->id, 'Ver', array('class' =>'btn btn-mini btn-primary'))}}
           </td>
   		  </tr>
   		@endforeach
   	</tbody>
-
   </table>
-  
+
+ </div>
+ {{HTML::link('/propuesta/add', 'Nueva Propuesta', array('class' =>'btn btn-primary'))}}
 </div>
 
 
