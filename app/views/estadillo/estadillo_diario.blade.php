71a324509318ddd04a259f56670f68c2f371a606
unknown <ajimenez@dyl-p002.ADSA.local>
2015-01-30 10:38:40 +0000

    Estadillos

--- app/views/estadillo/estadillo_diario.blade.php
+++ app/views/estadillo/estadillo_diario.blade.php
@@ -4,45 +4,54 @@
 
 
   <div class="container">
-  	<h1>Estadillos para el día
-      <form action="estadillos" id="formulario_estadillo">
-      <input type="text" name="fecha_pedido" id="fecha_pedido" value="{{date("d-m-Y",strtotime($fecha))}}"> 
+  	
+      <form action="estadillos" id="formulario_estadillo" class="form-horizontal">
+        <div class="form-group"> 
+         {{Form::label('fecha_pedido', 'Estadillos del día ', array('class' => 'col-sm-2 control-label'))}} 
+          <div class="col-sm-2">
+            <input type="text" class="form-control input-sm" name="fecha_pedido" id="fecha_pedido" value="{{date("d-m-Y",strtotime($fecha))}}"> 
+          </div>
+        </div>
       </form>
       
-    </h1>
-    <div class="datos_reales">
-      <h1>Procria</h1>
-    <table>
-      <tr>
-        <td>Jaula</td>
-        <td>Lote</td>
-        <td>Num. Peces</td>
-        <td>Peso Medio (gr.)</td>
-        <td>Biomasa (Kg.)</td>
-        <td>Pienso (Kg.)</td>
-        <td>Num. Tomas</td>
-        <td>Diámetro</td>
-        <td>Cantidad</td>
-      </tr>
+    
+    <div class="table-responsive">
+      <h5>Procria</h5>
+    <table class="table table-striped table-bordered">
+      <thead>
+       <tr>
+        <th class="text-center">Jaula</th>
+        <th class="text-center">Lote</th>
+        <th class="text-center">Num. Peces</th>
+        <th class="text-center">Peso Medio (gr.)</thd>
+        <th class="text-center">Biomasa (Kg.)</th>
+        <th class="text-center">Pienso (Kg.)</th>
+        <th class="text-center">Num. Tomas</th>
+        <th class="text-center">Diámetro</th>
+        <th class="text-center">Cantidad</th>
+       </tr>
+      </thead>
+      <tbody>
      @foreach($datos as $dato)
         <tr>
-          <td>{{$dato->jaula}}</td>
-          <td>{{$dato->lote}}</td>
-          <td>{{$dato->stock_count_ini}}</td>
-          <td>{{$dato->stock_avg_ini}}</td>
-          <td>{{$dato->stock_bio_ini}}</td>
-          <td>{{$dato->cantidad_toma}}</td>
-          <td><select @if($dato->cantidad_toma <= 0) disabled @endif >
+          <td class="text-center">{{$dato->jaula}}</td>
+          <td class="text-center">{{$dato->lote}}</td>
+          <td class="text-right">{{$dato->stock_count_ini}}</td>
+          <td class="text-right">{{$dato->stock_avg_ini}}</td>
+          <td class="text-right">{{$dato->stock_bio_ini}}</td>
+          <td class="text-right">{{$dato->cantidad_toma}}</td>
+          <td class="text-center"><select @if($dato->cantidad_toma <= 0) disabled @endif >
                  @for ($i=0; $i<3; $i++)
                  <option value={{$i}} 
                    @if ($i == $dato->num_tomas) selected @endif >{{$i}}</option>
                  @endfor
               </select></td>
-          <td>{{$dato->diametro_pienso}}</td>
-          <td>{{$dato->cantidad}}</td>
+          <td class="text-center">{{$dato->diametro_pienso}}</td>
+          <td class="text-right">{{$dato->cantidad}}</td>
 
         </tr>
      @endforeach
+   </tbody>
     </table>
     </div>
   </div>
