325d1ff5098c5410798c5f06388477ca180d0845
unknown <ajimenez@dyl-p002.ADSA.local>
2015-03-03 09:38:37 +0000

    Estadillos en la vista eliminamos la tabla de las dos granjas

--- app/views/estadillo/estadillo_diario.blade.php
+++ app/views/estadillo/estadillo_diario.blade.php
@@ -15,45 +15,7 @@
       </form>
       
     
-    <div class="table-responsive">
-      <h5>Procria</h5>
-    <table class="table table-striped table-bordered">
-      <thead>
-       <tr>
-        <th class="text-center">Jaula</th>
-        <th class="text-center">Lote</th>
-        <th class="text-center">Num. Peces</th>
-        <th class="text-center">Peso Medio (gr.)</thd>
-        <th class="text-center">Biomasa (Kg.)</th>
-        <th class="text-center">Pienso (Kg.)</th>
-        <th class="text-center">Num. Tomas</th>
-        <th class="text-center">Diámetro</th>
-        <th class="text-center">Cantidad</th>
-       </tr>
-      </thead>
-      <tbody>
-     @foreach($datos as $dato)
-        <tr>
-          <td class="text-center">{{$dato->jaula}}</td>
-          <td class="text-center">{{$dato->lote}}</td>
-          <td class="text-right">{{$dato->stock_count_ini}}</td>
-          <td class="text-right">{{$dato->stock_avg_ini}}</td>
-          <td class="text-right">{{$dato->stock_bio_ini}}</td>
-          <td class="text-right">{{$dato->cantidad_toma}}</td>
-          <td class="text-center"><select @if($dato->cantidad_toma <= 0) disabled @endif >
-                 @for ($i=0; $i<3; $i++)
-                 <option value={{$i}} 
-                   @if ($i == $dato->num_tomas) selected @endif >{{$i}}</option>
-                 @endfor
-              </select></td>
-          <td class="text-center">{{$dato->diametro_pienso}}</td>
-          <td class="text-right">{{$dato->cantidad}}</td>
-
-        </tr>
-     @endforeach
-   </tbody>
-    </table>
-    </div>
+    
     @foreach($estadillos_granja as $estadillo_granja)
      <div class="table-responsive">
        <h5>{{$estadillo_granja['granja']}} <span class="glyphicon glyphicon-print" fecha="{{date("d-m-Y",strtotime($fecha))}}" granja="{{$estadillo_granja['granja']}}"><a href="/acuifarm/public/ajax/estadillos/excel/{{date("d-m-Y",strtotime($fecha))}}/{{$estadillo_granja['granja']}}">Imprimir</a></span></h5>
