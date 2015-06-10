d2be0f0f5e911dcbb6bae9eb91387d8848b81ee0
unknown <ajimenez@dyl-p002.ADSA.local>
2015-06-10 18:39:06 +0100

    Importación stock almacenes automática

--- app/controllers/AlmacenController.php
+++ app/controllers/AlmacenController.php
@@ -174,86 +174,137 @@
                $ruta = Input::file('fichero')->getRealPath();
                Input::file('fichero')->move('public/', 'importacion.xls');
 
+               // Obtenemos la granja
+               $granja = Input::get('granja');
+
                $resultado_ajuste_stock = array() ;
-               $prueba = 'Hola';
-               Excel::load('public/importacion.xls', function($reader) use ($fecha_fichero, $resultado_ajuste_stock) {
+               //$prueba = 'Hola';
+               Excel::load('public/importacion.xls', function($reader) use ($fecha_fichero, $resultado_ajuste_stock, $granja) {
 
                  // reader methods
-                 // Probar esto: $reader->get()->groupBy('firstname');
-                $result=$reader->get();
-                foreach($result as $linea)
-                  {
-                    // Para cada linea, buscamos la cantidad de pienso en el almacén
-                    $estado_almacen = DB::select('Select almacenes.nombre as almacen, piensos.id, piensos.nombre as pienso, 
-                                             sum(cantidad) as cantidad, max(fecha)
-                                              from movimientos_almacenes, almacenes, piensos
-                                             where almacenes.id = movimientos_almacenes.almacen_id
-                                               and piensos.id   = movimientos_almacenes.pienso_id
-                                               and movimientos_almacenes.fecha <= ?
-                                               and almacenes.nombre = ?
-                                               and piensos.nombre = ?
-                                          group by almacenes.nombre, piensos.id, piensos.nombre order by almacenes.nombre, piensos.nombre desc', array($fecha_fichero, $linea->almacen, $linea->alimento));
+                 // Probar esto: 
+                $result= $reader->get()->groupBy('nombre')->groupby('Tipo_de_alimento');
+                //$result=$reader->get();
+                //print_r($result);
+                foreach($result as $almacen)
+                  { 
+                    //echo 'Nuevo almacen';
+                    foreach ($almacen as $tipo_alimento)
 
-                    $nuevo_registro_importacion_stock = new ImportacionStock();
-                    $nuevo_registro = new MovimientosAlmacen();
-                    // Obtenemos el id del almacen
-                    $objeto_almacen = Almacen::where('nombre', '=', $linea->almacen)->first();
-                    // Obtenemos el id del pienso
-                    $objeto_pienso = Pienso::where('nombre', '=', $linea->alimento)->first();
+                    {
+                      echo ' nuevo tipo de pienso';
+                      $nombre_almacen = '';
+                      $nombre_alimento = '';
+                      $cantidad = 0;
+                      $primera_fila = True;
+                       foreach ( $tipo_alimento as $linea)
+                       {
+                         //print_r($linea);
+                         if ($nombre_alimento <> $linea->tipo_de_alimento)
+                         {
+                           if (!$primera_fila)
+                            {
+                               echo ' Alimento: ' . $nombre_alimento . ' Almacén: ' . $nombre_almacen . ' cantidad: ' . $cantidad;
+                               // Aquí debemos actualizar los datos
+                               switch ($nombre_almacen) {
+                                 case 'MeH':
+                                    $almacen = 'Martín e Hijos';
+                                   break;
+                                 
+                                 case 'Salinetas':
+                                   if ($granja == 'Melenara')
+                                    {
+                                       $almacen = 'Melenara';
+                                    } 
+                                   else 
+                                    {
+                                       $almacen = 'Procria';
+                                    }
+                                   break;
+                               }
 
-                    if (count($estado_almacen) < 1){
-                         //echo 'No existe el pieso ' .$linea->alimento. ' en el almacen ' .$linea->almacen;
-                         if (count($objeto_pienso)>0){
-                              /*
-                           $nuevo_registro_importacion_stock->almacen_id =$objeto_almacen->id;
-                           $nuevo_registro_importacion_stock->pienso_id = $objeto_pienso->id;
-                           $nuevo_registro_importacion_stock->cantidad_acuifarm = 0;
-                           $nuevo_registro_importacion_stock->cantidad_fishtalk = $linea->cantidad;
-                           $nuevo_registro_importacion_stock->diferencia = $linea->cantidad;
-                           $nuevo_registro_importacion_stock->fecha = $fecha_fichero;
-                           $nuevo_registro_importacion_stock->save();
-                           */
-                           
-                           $nuevo_registro->almacen_id =$objeto_almacen->id;
-                           $nuevo_registro->tipo_movimiento = "Entrada";
-                           $nuevo_registro->descripcion ="Ajuste automático procedente de archivo";
-                           $nuevo_registro->pienso_id =$objeto_pienso->id;
-                           $nuevo_registro->cantidad =$linea->cantidad;
-                           $nuevo_registro->fecha = $fecha_fichero;
-                           $nuevo_registro->save();
+                               // Para cada linea, buscamos la cantidad de pienso en el almacén
+                               $estado_almacen = DB::select('Select almacenes.nombre as almacen, piensos.id, piensos.nombre as pienso, 
+                                                                    sum(cantidad) as cantidad, max(fecha)
+                                                               from movimientos_almacenes, almacenes, piensos
+                                                              where almacenes.id = movimientos_almacenes.almacen_id
+                                                                and piensos.id   = movimientos_almacenes.pienso_id
+                                                                and movimientos_almacenes.fecha <= ?
+                                                                and almacenes.nombre = ?
+                                                                and piensos.nombre = ?
+                                                           group by almacenes.nombre, piensos.id, piensos.nombre order by almacenes.nombre, piensos.nombre desc', array($fecha_fichero, $almacen, $nombre_alimento));
+
+                               $nuevo_registro_importacion_stock = new ImportacionStock();
+                               $nuevo_registro = new MovimientosAlmacen();
+                               // Obtenemos el id del almacen
+                               $objeto_almacen = Almacen::where('nombre', '=', $almacen)->first();
+                               // Obtenemos el id del pienso
+                               $objeto_pienso = Pienso::where('nombre', '=', $nombre_alimento)->first();
+
+                               if (count($estado_almacen) < 1)
+                                {
+                                 //echo 'No existe el pieso ' .$linea->alimento. ' en el almacen ' .$linea->almacen;
+                                 if (count($objeto_pienso)>0)
+                                  {
+                                    $nuevo_registro->almacen_id =$objeto_almacen->id;
+                                    $nuevo_registro->tipo_movimiento = "Entrada";
+                                    $nuevo_registro->descripcion ="Ajuste automático procedente de archivo";
+                                    $nuevo_registro->pienso_id =$objeto_pienso->id;
+                                    $nuevo_registro->cantidad =$cantidad;
+                                    $nuevo_registro->fecha = $fecha_fichero;
+                                    $nuevo_registro->save();
+                                  }
+                                } 
+                               else 
+                                {
+                                  foreach ($estado_almacen as $linea_almacen) 
+                                   {
+                                     $nuevo_registro->almacen_id =$objeto_almacen->id;
+                                     $nuevo_registro->tipo_movimiento = "Entrada";
+                                     $nuevo_registro->descripcion ="Ajuste automático procedente de archivo";
+                                     $nuevo_registro->pienso_id =$objeto_pienso->id;
+                                     $nuevo_registro->cantidad = $cantidad - $linea_almacen->cantidad;
+                                     $nuevo_registro->fecha = $fecha_fichero;
+                                     $nuevo_registro->save();
+                                   }
+                                }
+
+                               $nombre_almacen = $linea->nombre;
+                               $nombre_alimento = $linea->tipo_de_alimento;
+                               $cantidad = $linea->amount_kg;
+                            }
+                            else
+                            {
+                               $nombre_almacen = $linea->nombre;
+                               $nombre_alimento = $linea->tipo_de_alimento;
+                               $cantidad = $linea->amount_kg;
+                               $primera_fila = False;
+                            }
+                          }
+                          else
+                          {  
+                            $cantidad = $cantidad + $linea->amount_kg;
+                            
                          }
-                    } else {
-                         foreach ($estado_almacen as $linea_almacen) {
-                              /*
-                              $nuevo_registro_importacion_stock->almacen_id =$objeto_almacen->id;
-                              $nuevo_registro_importacion_stock->pienso_id = $objeto_pienso->id;
-                              $nuevo_registro_importacion_stock->cantidad_acuifarm = $linea_almacen->cantidad;
-                              $nuevo_registro_importacion_stock->cantidad_fishtalk = $linea->cantidad;
-                              $nuevo_registro_importacion_stock->diferencia = $linea->cantidad - $linea_almacen->cantidad;
-                              $nuevo_registro_importacion_stock->fecha = $fecha_fichero;
-                              $nuevo_registro_importacion_stock->save();
-                              */
-                              $nuevo_registro->almacen_id =$objeto_almacen->id;
-                              $nuevo_registro->tipo_movimiento = "Entrada";
-                              $nuevo_registro->descripcion ="Ajuste automático procedente de archivo";
-                              $nuevo_registro->pienso_id =$objeto_pienso->id;
-                              $nuevo_registro->cantidad = $linea->cantidad - $linea_almacen->cantidad;
-                              $nuevo_registro->fecha = $fecha_fichero;
-                              $nuevo_registro->save();
-                         }
+                         /*$nombre_almacen = $linea->nombre;
+                         $nombre_alimento = $linea->tipo_de_alimento;
+                         $cantidad = $cantidad + $linea->amount_kg;
+                         echo ' Alimento: ' . $nombre_alimento; */
 
-                         
+                       }
+                      //echo 'Almacén: ' . $nombre_almacen . ' Alimento: ' . $nombre_alimento . ' cantidad: ' . $cantidad;
                     }
+                    //echo 'Almacén ' . $nombre_almacen . ' Alimento: ' ; //. $linea->tipo_de_alimento . ' cantidad: '; //. $linea->Amount_[kg];
+                    /*
                     
                     
+                   */ 
                   }
                 
                   
                }); 
              
-             //Obtenemos el resultado de la importacion
-             //$resultado_ajuste_stock = ImportacionStock::where('fecha', '=', $fecha_fichero)->orderby('almacen_id')->get();
-              //return View::make('almacen.ajuste_confirmacion')->with('fecha', $fecha_fichero)->with('resultado', $resultado_ajuste_stock);  
+             
               return Redirect::to('almacenes/stock');
            }
           else 
