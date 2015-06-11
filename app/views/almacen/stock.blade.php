afe34fbb7f86a2a9dcf38ec61a45ee4ccd79b5c3
unknown <ajimenez@dyl-p002.ADSA.local>
2015-06-11 13:40:52 +0100

    Stock de almacenes

--- app/views/almacen/stock.blade.php
+++ app/views/almacen/stock.blade.php
@@ -3,26 +3,7 @@
 @section('content')
  <div class="container">
  	<h1>Stock de almacenes a {{$fecha}}</h1>
- 	<div class="table-responsive">
- 	<table class="table table-striped table-bordered">
-    <thead>
-    <tr>
-    	<th class="text-center">Almacén</th>
-    	<th class="text-center">Pienso</th>
-    	<th class="text-center">Cantidad</th>
-    </tr>
-    </thead>
-    <tbody>
-    @foreach($estado_almacen as $estado)
-    <tr>
-    <td class="text-center">{{$estado->almacen}}</td>
-    <td class="text-center">{{$estado->pienso}}</td>
-    <td class="text-center">{{$estado->cantidad}}</td>
-    </tr>
-    @endforeach
-    <tbody>
- 	</table>
- </div>
+ 	
 
  @foreach($stock_almacen as $stock)
 <div class="table-responsive">
